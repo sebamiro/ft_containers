8d2dbe6eee25d8d7e2ba77de4abc19f26670b2e2
sebamiro <seba.mi.21@gmail.com>
2023-02-19 20:08:19 +0100

    Stack 1.0 - iterator_traits 1.0

---
+++ Stack.hpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smiro <smiro@student.42barcelona>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 17:50:11 by smiro             #+#    #+#             */
+/*   Updated: 2023/02/18 17:50:12 by smiro            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stack>
+#include <iterator>
+#include <vector>
+#include <type_traits>
+#include <algorithm>
+
+
+namespace ft
+{
+
+template < class T, class Container = std::vector<T> > 
+class Stack
+{
+	public:
+	
+		typedef Container																	container_type;
+		typedef typename container_type::value_type				value_type;
+		typedef typename container_type::reference				reference;
+		typedef typename container_type::const_reference	const_reference;
+		typedef typename container_type::size_type				size_type;
+		
+	protected:
+		
+		container_type	c;
+
+	public:
+	
+		Stack(void)
+			: c() {};
+		Stack(const Stack & cpy)
+			: c(cpy.c) {};
+		Stack & operator=(const Stack & src) {
+			c = src.c;
+			return *this;
+		};
+		explicit Stack(const container_type & c)
+			: c(c) {};
+		~Stack() {};
+		
+		bool						empty(void) const { return c.empty();};
+		size_type				size(void) const { return c.size();};
+		reference				top(void) { return c.back();};
+		const_reference top(void) const { return c.back();};
+		void						push(const value_type & x) { c.push_back(x);};
+		void						pop(void) { c.pop_back(); };
+		
+		template <typename  value_type,typename container_type >
+		friend bool operator==(const Stack<value_type, container_type>& lhs, const Stack<value_type, container_type>& rhs){
+					return lhs.c == rhs.c;
+		};
+		
+		template <typename  value_type,typename container_type >
+		friend bool operator!=(const Stack<value_type, container_type>& lhs, const Stack<value_type, container_type>& rhs){
+					return lhs.c != rhs.c;
+		};
+		
+		template <typename  value_type,typename container_type >
+		friend bool operator>(const Stack<value_type, container_type>& lhs, const Stack<value_type, container_type>& rhs){
+					return lhs.c > rhs.c;
+		};
+		
+		template <typename  value_type,typename container_type >
+		friend bool operator>=(const Stack<value_type, container_type>& lhs, const Stack<value_type, container_type>& rhs){
+					return lhs.c >= rhs.c;
+		};
+		
+		template <typename  value_type,typename container_type >
+		friend bool operator<(const Stack<value_type, container_type>& lhs, const Stack<value_type, container_type>& rhs){
+					return lhs.c < rhs.c;
+		};
+		
+		template <typename  value_type,typename container_type >
+		friend bool operator<=(const Stack<value_type, container_type>& lhs, const Stack<value_type, container_type>& rhs){
+					return lhs.c <= rhs.c;
+		};
+};
+
+
+
+}
---
+++ iterator_traits.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator_traits.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smiro <smiro@student.42barcelona>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 19:00:13 by smiro             #+#    #+#             */
+/*   Updated: 2023/02/19 19:00:14 by smiro            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstddef>
+
+namespace ft
+{
+	
+	struct	random_access_iterator_tag { };
+	
+	template < class Iterator >
+	struct iterator_traits
+	{
+		typedef typename  Iterator::difference_type		difference_type;
+		typedef typename  Iterator::value_type				value_type;
+		typedef typename  Iterator::pointer						pointer;
+		typedef typename  Iterator::reference					reference;
+		typedef typename  Iterator::iterator_category	iterator_category;
+	};
+
+	template < class T >
+	struct iterator_traits<T*>
+	{
+		typedef	ptrdiff_t		difference_type;
+		typedef T				value_type;
+		typedef T*						pointer;
+		typedef T&					reference;
+		typedef ft::random_access_iterator_tag	iterator_category;
+	};
+
+	template < class Category, class T, class Distance = ptrdiff_t,
+							class Pointer = T*, class Reference = T& >
+	struct	iterator
+	{
+		typedef T					value_type;
+		typedef Distance	difference_type;
+		typedef	Pointer		pointer;
+		typedef Category	iterator_category;
+	};
+	
+		
+}
